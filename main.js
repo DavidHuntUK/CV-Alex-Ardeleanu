e37fac68ec72762eb89553323c7c020b5485505f
AlexArdGit <56441786+AlexArdGit@users.noreply.github.com>
2020-02-08 18:59:01 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function changePic() {
+    document.getElementById("profilePic").src = "P2.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "Profile.jpg";
+  }
